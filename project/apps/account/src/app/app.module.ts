2e2943822352b4aec5c1bb430a189d47476c010d
Timur <Timur>
2026-03-31 19:05:11 +0400

    3.14. Refactor AppModule

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { ConfigAccountModule } from '@project/config-account';
-import { getMongoConnectionString } from '@project/helpers';
+import {
+  ConfigAccountModule,
+  getMongooseOptions,
+} from '@project/config-account';
 
 import { AuthenticationModule } from './authentication/authentication.module';
 
@@ -11,20 +12,7 @@ import { AuthenticationModule } from './authentication/authentication.module';
   imports: [
     AuthenticationModule,
     ConfigAccountModule,
-    // TODO: refactor
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        uri: getMongoConnectionString({
-          username: config.getOrThrow<string>('mongo.user'),
-          password: config.getOrThrow<string>('mongo.password'),
-          host: config.getOrThrow<string>('mongo.host'),
-          port: config.getOrThrow<number>('mongo.port'),
-          authDatabase: config.getOrThrow<string>('mongo.authBase'),
-          databaseName: config.getOrThrow<string>('mongo.name'),
-        }),
-      }),
-    }),
+    MongooseModule.forRootAsync(getMongooseOptions()),
   ],
   controllers: [],
   providers: [],
--- project/libs/shared/config/account/src/index.ts
+++ project/libs/shared/config/account/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/config-account.module';
+export { ConfigAccountModule } from './lib/config-account.module';
+export { getMongooseOptions } from './lib/get-mongoose-options';
