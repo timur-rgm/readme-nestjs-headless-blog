bb0d7082bd49aab0794b6d0596e7e724eb338de5
Timur <timur.rgm@gmail.com>
2026-08-06 15:47:54 +0400

    feat(types): add jwt token contracts

--- project/libs/shared/types/src/index.ts
+++ project/libs/shared/types/src/index.ts
@@ -1,17 +1,18 @@
-export { PostType } from './lib/post-type.enum';
-export { SortDirection } from './lib/sort-direction.enum';
-
 export type { AuthUser } from './lib/auth-user.interface';
 export type { Comment } from './lib/comment.interface';
 export type { Like } from './lib/like.interface';
 export type { PaginationResult } from './lib/pagination.interface';
+export { PostType } from './lib/post-type.enum';
 export type {
-  PostBase,
   LinkPost,
-  QuotePost,
   PhotoPost,
   Post,
+  PostBase,
+  QuotePost,
   TextPost,
   VideoPost,
 } from './lib/post.interface';
+export { SortDirection } from './lib/sort-direction.enum';
+export type { Token } from './lib/token.interface';
+export type { TokenPayload } from './lib/token-payload.interface';
 export type { User } from './lib/user.interface';
