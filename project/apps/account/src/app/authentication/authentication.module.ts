e3917790cba4221878cc479d18c644cc590d6eba
Timur <timur.rgm@gmail.com>
2026-08-07 11:30:31 +0400

    feat(account): apply jwt module

--- project/apps/account/src/app/authentication/authentication.module.ts
+++ project/apps/account/src/app/authentication/authentication.module.ts
@@ -1,5 +1,9 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { getJwtOptions } from '@project/config-account';
 
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
@@ -8,6 +12,10 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions
+    }),
     MongooseModule.forFeature([{ name: UserModel.name, schema: UserSchema }]),
   ],
   controllers: [AuthenticationController],
