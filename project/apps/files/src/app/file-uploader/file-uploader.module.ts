b0723cbc6001c49d7a90a697d5fd14a31fd0a381
Timur <timur.rgm@gmail.com>
2026-08-23 10:26:27 +0400

    feat(files): configure static file serving

--- project/apps/files/src/app/file-uploader/file-uploader.module.ts
+++ project/apps/files/src/app/file-uploader/file-uploader.module.ts
@@ -1,9 +1,34 @@
+import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { FileUploaderController } from './file-uploader.controller';
 import { FileUploaderService } from './file-uploader.service';
 
+const SERVE_ROOT = '/static';
+
 @Module({
+  imports: [
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const rootPath = configService.getOrThrow<string>(
+          'application.uploadDirectory',
+        );
+
+        return [
+          {
+            rootPath,
+            serveRoot: SERVE_ROOT,
+            serveStaticOptions: {
+              fallthrough: true,
+              etag: true,
+            },
+          },
+        ];
+      },
+    }),
+  ],
   providers: [FileUploaderService],
   controllers: [FileUploaderController],
 })
