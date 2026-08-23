d58c7392c545e902ee9504b615c4ceb4ca1402a0
Timur <timur.rgm@gmail.com>
2026-08-23 07:50:14 +0400

    feat(files): add file uploader controller [wip]

---
+++ project/apps/files/src/app/file-uploader/file-uploader.controller.ts
@@ -0,0 +1,21 @@
+import 'multer';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { FileUploaderService } from './file-uploader.service';
+
+@Controller('files')
+export class FileUploaderController {
+  constructor(private readonly fileUploaderService: FileUploaderService) {}
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file'))
+  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileUploaderService.saveFile(file);
+  }
+}
