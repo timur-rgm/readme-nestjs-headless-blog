0ec3c023672b2301681004783a8299f18e42c356
Timur <timur.rgm@gmail.com>
2026-08-24 15:17:16 +0400

    feat(files): update service

---
+++ project/apps/files/src/app/file-uploader/errors/index.ts
@@ -0,0 +1,2 @@
+export { FileNotFoundError } from './file-not-found.error';
+export { UnsupportedFileTypeError } from './unsupported-file-type.error';
--- project/apps/files/src/app/file-uploader/file-uploader.service.ts
+++ project/apps/files/src/app/file-uploader/file-uploader.service.ts
@@ -8,17 +8,58 @@ import { join } from 'node:path';
 import { randomUUID } from 'node:crypto';
 import { writeFile } from 'node:fs/promises';
 
+import type { StoredFile } from '@project/types';
+
+import { FileEntity } from './file.entity';
+import { FileRepository } from './file.repository';
+import { FileNotFoundError, UnsupportedFileTypeError } from './errors';
+
 @Injectable()
 export class FileUploaderService {
   private readonly logger = new Logger(FileUploaderService.name);
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly fileRepository: FileRepository,
+  ) {}
 
-  public async saveFile(file: Express.Multer.File): Promise<string> {
+  public async saveFile(file: Express.Multer.File): Promise<FileEntity> {
+    const { filename, path, subDirectory } = await this.writeFile(file);
+
+    const fileEntity = FileEntity.fromObject({
+      originalName: file.originalname,
+      subDirectory,
+      size: file.size,
+      mimetype: file.mimetype,
+      hashName: filename,
+      path,
+    });
+
+    return this.fileRepository.save(fileEntity);
+  }
+
+  public async getFile(fileId: string): Promise<FileEntity> {
+    const fileEntity = await this.fileRepository.findById(fileId);
+
+    if (!fileEntity) {
+      throw new FileNotFoundError(`File with id ${fileId} not found.`);
+    }
+
+    return fileEntity;
+  }
+
+  private async writeFile(file: Express.Multer.File): Promise<StoredFile> {
     const fileExtension = extension(file.mimetype);
-    const filename = `${randomUUID()}.${fileExtension}`;
 
-    const uploadDirectoryPath = this.getUploadDirectoryPath();
+    if (!fileExtension) {
+      throw new UnsupportedFileTypeError('Unsupported file MIME type');
+    }
+
+    const filename = `${randomUUID()}.${fileExtension}`;
+    const [year, month] = dayjs().format('YYYY MM').split(' ');
+    const subDirectory = join(year, month);
+
+    const uploadDirectoryPath = this.getUploadDirectoryPath(subDirectory);
     const destinationFilePath = this.getDestinationFilePath(
       uploadDirectoryPath,
       filename,
@@ -34,15 +75,19 @@ export class FileUploaderService {
       throw new Error(`Can't save file`);
     }
 
-    return destinationFilePath;
+    return {
+      filename,
+      fileExtension,
+      subDirectory,
+      path: destinationFilePath,
+    };
   }
 
-  private getUploadDirectoryPath(): string {
-    const [year, month] = dayjs().format('YYYY MM').split(' ');
+  private getUploadDirectoryPath(subDirectory: string): string {
     const uploadDirectory = this.configService.getOrThrow<string>(
       'application.uploadDirectory',
     );
-    return join(uploadDirectory, year, month);
+    return join(uploadDirectory, subDirectory);
   }
 
   private getDestinationFilePath(
